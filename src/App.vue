1d11d19d302ebc3faade3c75de5def9ad8709935
863978745@qq.com <863978745@qq.com>
2026-01-31 23:03:40 +0800

    26 01 31

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,12 @@
 </script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-  <el-button type="primary">Primary</el-button>
+  <router-view />
+  <div class="box">1111</div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.box {
+  color: $priceColor;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,34 @@
-import { createRouter, createWebHistory } from 'vue-router'
+// createRouter：创建router实例对象
+// createWebHistory：创建history模式的路由
+
+import { createRouter, createWebHistory } from "vue-router";
+import Login from "@/views/Login/Index.vue";
+import Layout from "@/views/Layout/Index.vue";
+import Home from "@/views/Home/Index.vue";
+import Category from "@/views/Category/Index.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes: [
+    {
+      path: "/",
+      component: Layout,
+      children: [
+        {
+          path: "",
+          component: Home,
+        },
+        {
+          path: "category",
+          component: Category,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      component: Login,
+    },
+  ],
+});
 
-export default router
+export default router;
