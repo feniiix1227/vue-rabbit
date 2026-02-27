f1713918ee06b38d40233dad62061def87e3dde6
863978745@qq.com <863978745@qq.com>
2026-02-27 21:23:32 +0800

    pinia

--- src/views/Layout/Index.vue
+++ src/views/Layout/Index.vue
@@ -3,6 +3,8 @@ import LayoutNav from "./components/LayoutNav.vue";
 import LayoutHeader from "./components/LayoutHeader.vue";
 import LayoutFooter from "./components/LayoutFooter.vue";
 import LayoutFixed from "./components/LayoutFixed.vue";
+
+// 触发获取导航列表的函数
 </script>
 
 <template>
