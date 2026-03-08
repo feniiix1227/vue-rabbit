671921e5f2fca42d082ca2bab8ecb1c408645426
863978745@qq.com <863978745@qq.com>
2026-03-08 15:47:27 +0800

    260308

--- src/main.js
+++ src/main.js
@@ -6,9 +6,15 @@ import router from "./router";
 
 import "@/styles/common.scss";
 
+// 引入对应的懒加载指令
+import { lazyPlugin } from "@/directives";
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+
+// 使用懒加载指令
+app.use(lazyPlugin);
 
 app.mount("#app");
